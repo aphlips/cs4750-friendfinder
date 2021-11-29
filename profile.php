db69a08fc1f33746b7335aa7d1cfb67c1aeaace4
Alan Phlips <77595501+aphlips@users.noreply.github.com>
2021-11-29 18:06:00 -0500

    profile for jaws

--- profile.php
+++ profile.php
@@ -26,7 +26,18 @@
     <h1>
         <?php 
             session_start();
-            $db = mysqli_connect('localhost', 'root', '', '4750_project');
+            //$db = mysqli_connect('localhost', 'root', '', '4750_project');
+        
+            //JAWSDB stuff
+            $url = getenv('JAWSDB_URL');
+            $dbparts = parse_url($url);
+            $hostname = $dbparts['host'];
+            $username = $dbparts['user'];
+            $password = $dbparts['pass'];
+            $database = ltrim($dbparts['path'],'/');
+
+            $db = new mysqli($hostname, $username, $password, $database);
+        
             $sName = $db->query("SELECT student.name FROM student WHERE student_id='$_SESSION[studentID]'")->fetch_object()->name;
             echo $sName;
         ?>
